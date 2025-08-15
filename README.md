55652cf09a207bbf71346ed269b40592896c4443
Alex Sh <shalex.work@gmail.com>
2025-08-15 13:10:36 +0300

    Support installing and running jetson sdk on Ubuntu or Ubuntu WSL

---
+++ README.md
@@ -0,0 +1,6 @@
+# jetson-tools
+
+Collection of useful tools for NVIDIA Jetson platform
+
+## Jetson Setup
+* [Jetson SDK Manager](jetson-sdk-manager-runner/) for Ubuntu/WSL installer and runner
---
+++ jetson-sdk-manager-runner/jetson_sdk_manager_runner.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if grep -qEi "(Microsoft|WSL)" /proc/version; then
+    WSL=true
+else
+    WSL=false
+fi
+
+if [ "$WSL" = true ]; then
+    cat << EOF
+Use are running on WSL
+Execute on Windows Host machine:
+--------------------------------
+winget install --interactive --exact dorssel.usbipd-win1 # Only if not already installed
+usbipd.exe list # find BUSID starting from 0955
+usbipd.exe bind --busid <BUSID> --force
+usbipd.exe attach --wsl --busid=<BUSID> --auto-attach
+EOF
+
+    read -p "Type 'Go' or 'g' to continue: " user_input
+    while [[ "$user_input" != "Go" && "$user_input" != "g" ]]; do
+        echo "Invalid input. Please type 'Go' or 'g' to continue."
+        read -p "Execute and type 'Go' or 'g' to continue: " user_input
+    done
+fi
+
+if ! command -v sdkmanager &> /dev/null; then
+    sudo apt update
+
+    if [ "$WSL" = true ]; then
+        sudo apt install -y wslu linux-tools-virtual hwdata
+    fi
+
+    ubuntu_version=$(lsb_release -rs | sed 's/\.//g')
+    formatted_version="ubuntu${ubuntu_version}"
+
+    wget https://developer.download.nvidia.com/compute/cuda/repos/$formatted_version/x86_64/cuda-keyring_1.1-1_all.deb
+    sudo dpkg -i cuda-keyring_1.1-1_all.deb
+    rm -rf cuda-keyring_1.1-1_all.deb
+    sudo apt -y install sdkmanager
+fi
+
+sdkmanager
